42d4c556d2174ba16c6f9dee732baab7745063a6
Toast-irl <abn5401@psu.edu>
2025-11-20 17:14:18 -0500

    Refactor variables and calculations in Cruise_performance.m
    Added variables for weight, velocity, drag calculations, and power conversion.

--- Cruise_performance.m
+++ Cruise_performance.m
@@ -1,7 +1,31 @@
 %% Variables
+W = 13000; %lbs Weight
+v_kt_cal = 260; % Calibrated
+v_kt = 300.077; % Velocity in Knots
 
-v_kt = 260; % Velocity in Knots
-v_ft = 0.592484 * v_kt; % Velocity in Ft/s
-e = 0.75;
+v_ft = v_kt/0.592484; % Velocity in ft/s
+v_ft_cal = v_kt_cal/0.592484; % Velocity calibrated in Ft/s
+
+e = 0.055; %oswald eff.
+rho = 0.001183; %slug/ft^3 @ 10,000ft
+f = 10.78; % ft^2 flat plate area
+
+D_p = 0.5*rho*(v_ft_cal)^2*f; % Parasite Drag
+S = 169; %ft^2 Wing Area
+b = 35; %span
+AR = (b^2)/S;
+S_wet = 1941.68; % ft^2 S wetted
+P_i = (W^2)/(2*rho*S*v_ft_cal);
+
+
+CL = W/(0.5*rho*(v_ft_cal^2)*S)
+CDi = (1/(pi*e*AR))*(CL^2)
+CDe = f/S
+
+CD = CDi+CDe
+
+T = W*(CD/CL);
+P = T*v_ft_cal;
+P_hp = (P * 1.818182*10^-3)*1.15 % Convert to hp
 
 
