dc53310d37314d9ca0fd96e020aca30858a0125b
Toast-irl <abn5401@psu.edu>
2025-12-04 13:20:30 -0500

    Refactor Cruise_performance.m with updated parameters

--- Cruise_performance.m
+++ Cruise_performance.m
@@ -7,24 +7,33 @@ v_ft = v_kt/0.592484; % Velocity in ft/s
 v_ft_cal = v_kt_cal/0.592484; % Velocity calibrated in Ft/s
 
 e = 0.055; %oswald eff.
-rho = 0.001183; %slug/ft^3 @ 10,000ft
+eta_p = 0.85;
+rho = 0.001818; %slug/ft^3 @ 10,000ft
 f = 10.78; % ft^2 flat plate area
+
+tc = 0.18;
 %%
 
 D_p = 0.5*rho*(v_ft)^2*f; % Parasite Drag
 S = 169; %ft^2 Wing Area
-b = 35; %span
+b = 32.17; %span     % Incorrect Span
 AR = (b^2)/S;
 S_wet = 1941.68; % ft^2 S wetted
 P_i = (W^2)/(2*rho*S*v_ft);
 
-
+CD0 = 0.055;
 CL = W/(0.5*rho*(v_ft^2)*S)
-CDi = (1/(pi*e*AR))*(CL^2)
-CDe = f/S
 
-CD = CDi+CDe
+term1 = 0.55 .* tc .* (CL.^5) ./ (pi .* AR);
+term2 = 0.55 .* tc ./ ( (CL.^2.4) .* pi .* AR );
+e = -1+(1 ./ ( 0.618 + term1 + term2 ));
 
+k=(1/(pi*e*AR))
+CDi = k*(CL^2)
+CDe = f/S;
+
+CD = CDi+CD0;
+ 
 T = W*(CD/CL);
 P = T*v_ft;
-P_hp = (P * 1.818182*10^-3)*1.15 % Convert to hp
+P_hp = P/(eta_p*550) % Convert to hp
